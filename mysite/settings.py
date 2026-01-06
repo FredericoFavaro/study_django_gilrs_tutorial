0b1665d9b6ff311985beea11ad201428afd7732d
Frederico Fávaro <fredericofavaro@gmail.com>
2026-01-06 22:28:57 +0000

    feat:chage os with path

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     STATIC_ROOT = BASE_DIR / 'static'
